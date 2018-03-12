72bd5671b4cc15b9c5019b26eba9e7c91157d899
alan.gomes <alan.gomes@EPSJV-SINF11.fiocruz.local>
2018-03-12 11:45:40 -0300

    correção connectionFactory - conexão com a base do heroku

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -19,25 +19,25 @@ function createDBConnection(){
         });
     }
 
-    // if(process.env.NODE_ENV == 'production') {
+     if(process.env.NODE_ENV == 'production') {
+         return mysql.createConnection({
+             host : 'us-cdbr-iron-east-05.cleardb.net',
+             user : 'b8fe8b9c15a6b2',
+             password : 'cb274d69',
+             database : 'heroku_707cc8bf0bc6e09'
+         });
+     }
+    
+    // if (process.env.NODE_ENV == 'production') {
+    //     var url = process.env.CLEARDB_DATABASE_URL;
+    //     var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
     //     return mysql.createConnection({
-    //         host : 'us-cdbr-iron-east-05.cleardb.net',
-    //         user : 'b8fe8b9c15a6b2',
-    //         password : 'cb274d69',
-    //         database : 'heroku_707cc8bf0bc6e09'
+    //         host:grupos[3],
+    //         user:grupos[1],
+    //         password:grupos[2],
+    //         database:grupos[4]
     //     });
     // }
-    
-    if (process.env.NODE_ENV == 'production') {
-        var url = process.env.CLEARDB_DATABASE_URL;
-        var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
-        return mysql.createConnection({
-            host:grupos[3],
-            user:grupos[1],
-            password:grupos[2],
-            database:grupos[4]
-        });
-    }
 }       
 
 module.exports = function(){
